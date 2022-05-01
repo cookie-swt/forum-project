fdb57986961a7f98b5c57c07e35e3ee23f12bf18
cookie-swt <1057792897@qq.com>
2022-05-01 18:07:57 +0800

    master

--- MyForum/forum/models.py
+++ MyForum/forum/models.py
@@ -8,21 +8,21 @@ class User(AbstractUser):
     pass
 
 class Posting(models.Model):
-    landlord_id=models.OneToOneField(User,on_delete=models.DO_NOTHING)
+    landlord_id=models.OneToOneField(User,on_delete=models.CASCADE)
     p_Title=models.CharField(max_length=20)
     p_Des=models.CharField(max_length=200)
     p_Date=models.DateField()
     p_See=models.BooleanField(default=True)
 
 class Comment(models.Model):
-    c_User_id=models.OneToOneField(User,on_delete=models.DO_NOTHING)
+    c_User_id=models.OneToOneField(User,on_delete=models.CASCADE)
     c_Content=models.CharField(max_length=500)
     c_Date=models.DateField()
     c_Likes=models.IntegerField(default=0)
-    c_Posting_id=models.ForeignKey(Posting,on_delete=models.DO_NOTHING)
+    c_Posting_id=models.ForeignKey(Posting,on_delete=models.CASCADE)
 
 class Collection(models.Model):
-    User_id=models.ForeignKey(User,on_delete=models.DO_NOTHING)
-    Posting_id=models.ForeignKey(Posting,on_delete=models.DO_NOTHING)
+    User_id=models.ForeignKey(User,on_delete=models.CASCADE)
+    Posting_id=models.ForeignKey(Posting,on_delete=models.CASCADE)
 
 
